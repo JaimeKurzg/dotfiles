61d084d16535a2cd7c9503c4eb4a9de5c6bfb809
Jaime Kurzweg <jkurzweg10@gmail.com>
2024-03-19 17:10:39 -0600

    init. just accidentally deleted sh.nix, so git now

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Main Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    xremap-flake.url = "github:xremap/nix-flake";
+	firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  	let 
+		lib = nixpkgs.lib;
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
+	in {
+	nixosConfigurations = {
+		nixos = nixpkgs.lib.nixosSystem {
+			inherit system;
+			specialArgs = { inherit inputs; };
+			modules = [ ./configuration.nix ];
+		};
+	  };
+	homeConfigurations = {
+		jaimek = home-manager.lib.homeManagerConfiguration {
+			inherit pkgs;
+			extraSpecialArgs = { inherit inputs; };
+			modules = [ ./home.nix ];
+		};
+	};
+	};
+}
---
+++ home.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, inputs, ... }:
+
+{
+imports = [];
+#	++ homeManagerModules;
+
+home.username = "jaimek";
+home.homeDirectory = "/home/jaimek";
+
+# You should not change this value, even if you update Home Manager. If you do
+# want to update the value, then make sure to first check the Home Manager
+# release notes.
+home.stateVersion = "23.11"; # Please read the comment before changing.
+
+home.packages = [
+	pkgs.spotify-player
+	pkgs.tridactyl-native
+];
+
+# Home Manager is pretty good at managing dotfiles. The primary way to manage
+# plain files is through 'home.file'.
+home.file = {
+	#".config/i3/config".source = ./i3-config;
+};
+
+home.sessionVariables = {
+	EDITOR = "nvim";
+};
+
+programs.git = {
+	enable = true;
+	userName = "Jaime Kurzweg";
+	userEmail = "jkurzweg10@gmail.com";
+	extraConfig  = {
+		init.defaultBranch = "master";
+	};
+};
+
+programs.firefox = {
+	enable = true;
+	profiles.jaimek = {
+		extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
+			ublock-origin
+			tridactyl
+			dictionaries
+			sponsorblock
+		];
+	};
+};
+
+# Let Home Manager install and manage itself.
+	programs.home-manager.enable = true;
+}
+
