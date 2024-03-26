58f071c7afd7d547c6a15aee133a3eebbe6cc518
Jaime Kurzweg <jkurzweg10@gmail.com>
2024-03-26 17:25:23 -0600

    Revert "got spotify to work better"
    This reverts commit a878be9054edb520155315b79c65d1973f5fdde1.

--- homeManagerModules/spotify.nix
+++ homeManagerModules/spotify.nix
@@ -2,4 +2,13 @@
 	home.packages = with pkgs; [
 		spotify-tui
 	];
+
+	services.spotifyd = {
+		enable = true;
+		settings.global = {
+			username = "jkurzweg10@gmail.com";
+			password_cmd = "pass spotify";
+		};
+
+	};
 }
