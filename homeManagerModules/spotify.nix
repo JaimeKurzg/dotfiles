a878be9054edb520155315b79c65d1973f5fdde1
Jaime Kurzweg <jkurzweg10@gmail.com>
2024-03-26 15:00:50 -0600

    got spotify to work better

--- homeManagerModules/spotify.nix
+++ homeManagerModules/spotify.nix
@@ -2,13 +2,4 @@
 	home.packages = with pkgs; [
 		spotify-tui
 	];
-
-	services.spotifyd = {
-		enable = true;
-		settings.global = {
-			username = "jkurzweg10@gmail.com";
-			password_cmd = "pass spotify";
-		};
-
-	};
 }
