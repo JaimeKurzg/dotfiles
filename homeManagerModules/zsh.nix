46e2b2442f3a40a6ffa67e827296db3d54cf381d
Jaime Kurzweg <jkurzweg10@gmail.com>
2024-04-04 17:04:01 -0600

    made zsh the default nix-shell

--- homeManagerModules/zsh.nix
+++ homeManagerModules/zsh.nix
@@ -32,6 +32,16 @@
 		enableAutosuggestions = true;
 		enableCompletion = true;
 		defaultKeymap = "viins";
+		plugins = [{
+			name = "zsh-nix-shell";
+			file = "nix-shell.plugin.zsh";
+			src = pkgs.fetchFromGitHub {
+				owner = "chisui";
+				repo = "zsh-nix-shell";
+				rev = "v0.8.0";
+				sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+			};
+		}];
 		shellAliases = {
 			"clip" = "xclip -selection clipboard";
 			"cat" = "bat";
@@ -39,6 +49,7 @@
 		initExtra = ''
 			bindkey -v '^?' backward-delete-char
 			eval "$(zoxide init zsh --cmd cd)"
+			export NIX_BUILD_SHELL=zsh
 			'';
 	};
 	programs.tmux = {
