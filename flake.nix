66fec4a21ac5df0deaa104b928bebb6e32f44b8c
Jaime Kurzweg <jkurzweg10@gmail.com>
2024-04-13 13:16:13 -0600

    Added unstable pkgs

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
+		nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 	    musnix.url = "github:musnix/musnix";
 		sops-nix.url = "github:Mic92/sops-nix";
 		home-manager.url = "github:nix-community/home-manager/release-23.11";
@@ -15,16 +16,20 @@
 		};
 	};
 
-	outputs = { self, nixpkgs, home-manager, stylix, ... }@inputs:
-		let 
+	outputs = { self, nixpkgs, home-manager, nixpkgs-unstable, stylix, ... }@inputs:
+	let 
 		lib = nixpkgs.lib;
-	system = "x86_64-linux";
-	pkgs = nixpkgs.legacyPackages.${system};
+		unstable = nixpkgs-unstable.legacyPackages.${system};
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
 	in {
 		nixosConfigurations = {
 			nixos = nixpkgs.lib.nixosSystem {
 				inherit system;
-				specialArgs = { inherit inputs; };
+				specialArgs = { 
+					inherit inputs; 
+					inherit unstable;
+				};
 				modules = [
 					./configuration.nix 
 					home-manager.nixosModules.home-manager
