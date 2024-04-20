b2825eed0a0019121b5f3deaec679df4f25b6330
Jaime Kurzweg <jkurzweg10@gmail.com>
2024-04-20 16:03:34 -0600

    add git-hub desktop

--- configuration.nix
+++ configuration.nix
@@ -87,19 +87,10 @@
 	security.rtkit.enable = true;
 	services.pipewire = {
 		enable = true;
+		pulse.enable = true;
 		alsa.enable = true;
 		alsa.support32Bit = true;
-
-# If you want to use JACK applications, uncomment this
-#jack.enable = true;
-
-# use the example session manager (no others are packaged yet so this is enabled by default,
-# no need to redefine it in your config for now)
-#media-session.enable = true;
 	};
-
-# Enable touchpad support (enabled default in most desktopManager).
-# services.xserver.libinput.enable = true;
 
 # Define a user account. Don't forget to set a password with ‘passwd’.
 	users.users.jaimek = {
@@ -147,6 +138,8 @@ nix.settings = {
 		gimp
 		lutris
 		unstable.jujutsu
+
+		github-desktop
 	];
 
 	programs.steam.enable = true;
