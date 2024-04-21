2f3683f279f530f7500a1c2e411f85939740ac6c
Jaime Kurzweg <jkurzweg10@gmail.com>
2024-04-21 09:47:20 -0600

    made two finger tap default

--- homeManagerModules/wm/sway.nix
+++ homeManagerModules/wm/sway.nix
@@ -17,19 +17,19 @@
 			terminal = "kitty"; 
 
 			startup = [
-				{command = ''exec swaymsg "workspace 3; exec qutebrowser"'';}
+				{command = ''swaymsg "workspace 3; exec qutebrowser"'';}
 			];
 			keybindings = let
 				modifier = config.wayland.windowManager.sway.config.modifier;
 				mod2 = "Mod4";
 			in lib.mkOptionDefault {
 				"${modifier}+t" = "exec ${pkgs.kitty}/bin/kitty";
-				"${modifier}+b" = "workspace number 3; exec ${pkgs.qutebrowser}/bin/qutebrowser";
+				"${modifier}+b" = "workspace --no-auto-back-and-forth number 3; exec ${pkgs.qutebrowser}/bin/qutebrowser";
 				"${modifier}+q" = "kill";
 				"${modifier}+d" = "exec ${pkgs.rofi}/bin/rofi -show drun";
 				"${modifier}+0" = "workspace number 10";
 				"${modifier}+Shift+0" = "move container to workspace number 10";
-				"${modifier}+w" = "workspace number 10; exec kitty -e spotify_player";
+				"${modifier}+w" = "workspace --no-auto-back-and-forth number 10; exec kitty -e spotify_player";
 				"XF86MonBrightnessDown" = "exec brightnessctl set 5%-";
 				"XF86MonBrightnessUp" = "exec brightnessctl set 5%+";
 			};
@@ -38,6 +38,13 @@
 			bindsym XF86AudioRaiseVolume exec 'wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+'
 			bindsym XF86AudioLowerVolume exec 'wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%-'
 			bindsym XF86AudioMute exec 'wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle'
+
+
+			for_window [class="Booligrams"] floating enable
+			input 2362:14408:PIXA3848:01_093A:3848_Touchpad {
+				click_method clickfinger
+				middle_emulation enabled
+			}
 			'';
 	};
 
