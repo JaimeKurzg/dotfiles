99cde7a39ce53717ec03eac55546f0c2880dbeef
Jaime Kurzweg <jkurzweg10@gmail.com>
2024-05-16 18:10:39 -0600

    added =a keymap nvim

--- homeManagerModules/nvim/default.nix
+++ homeManagerModules/nvim/default.nix
@@ -2,17 +2,35 @@
 {
 	options = {
 		nvim.enable = 
-		lib.mkEnableOption "use my neovim config";
+			lib.mkEnableOption "use my neovim config";
 	};
 	config = lib.mkIf config.nvim.enable {
 
-	home.packages = [
-		pkgs.ripgrep
+		home.packages = [
+			pkgs.ripgrep
 
-		( inputs.nixvim.legacyPackages.${pkgs.system}.makeNixvimWithModule {
-		 	module = import ./nvim.nix { inherit pkgs; } ;
-		} )
+				( inputs.nixvim.legacyPackages.${pkgs.system}.makeNixvimWithModule {
+				  module = 
 
-	];
-};
+				  {...}:{
+				  imports = [
+					  ./plugins
+					  ./options.nix
+					  ./keymaps.nix
+					  ./godot-nvim.nix
+				  ];
+				  config = {
+					  viAlias = true;
+					  vimAlias = true;
+
+					  globals = {
+					  mapleader = " ";
+					  };
+				  };
+
+				  };
+				  } )
+
+		];
+	};
 }
--- homeManagerModules/nvim/keymaps.nix
+++ homeManagerModules/nvim/keymaps.nix
@@ -1,89 +1,92 @@
 
 {
-		keymaps = [
-		{
-			key = ";";
-			mode = "n";
-			action = ":";
-		}
-		{
-			mode = "v";
-			key = "J";
-			action = ":m '>+1<CR>gv=gv";
-		}
-		{
-			mode = "v";
-			key = "K";
-			action = ":m '<-2<CR>gv=gv";
-		}
+	keymaps = [
+	{
+		key = ";";
+		mode = "n";
+		action = ":";
+	}
+	{
+		mode = "v";
+		key = "J";
+		action = ":m '>+1<CR>gv=gv";
+	}
+	{
+		mode = "v";
+		key = "K";
+		action = ":m '<-2<CR>gv=gv";
+	}
 
-		{
-			mode = "n";
-			key = "J";
-			action = "mzJ`z";
-		}
-		{
-			mode = "n";
-			key = "<C-d>";
-			action = "<C-d>zz";
-		}
-		{
-			mode = "n";
-			key = "<C-u>";
-			action = "<C-u>zz";
-		}
-		{
-			mode = "n";
-			key = "n";
-			action = "nzzzv";
-		}
-		{
-			mode = "n";
-			key = "N";
-			action = "Nzzzv";
-		}
+	{
+		mode = "n";
+		key = "J";
+		action = "mzJ`z";
+	}
+	{
+		mode = "n";
+		key = "<C-d>";
+		action = "<C-d>zz";
+	}
+	{
+		mode = "n";
+		key = "<C-u>";
+		action = "<C-u>zz";
+	}
+	{
+		mode = "n";
+		key = "n";
+		action = "nzzzv";
+	}
+	{
+		mode = "n";
+		key = "N";
+		action = "Nzzzv";
+	}
 
-		{
-			mode = "x";
-			key = "<leader>p";
-			action = ''"_dP'';
-		}
+	{
+		mode = "x";
+		key = "<leader>p";
+		action = ''"_dP'';
+	}
 
-		{
-			mode = ["n" "v"];
-			key = "<leader>y";
-			action = ''"+y'';
-		}
-		{
-			mode = "n";
-			key = "<leader>Y";
-			action = ''"+Y'';
-		}
+	{
+		mode = ["n" "v"];
+		key = "<leader>y";
+		action = ''"+y'';
+	}
+	{
+		mode = "n";
+		key = "<leader>Y";
+		action = ''"+Y'';
+	}
 
-		{
-			mode = ["n" "v"];
-			key = "<leader>d";
-			action = ''"_d'';
-		}
+	{
+		mode = ["n" "v"];
+		key = "<leader>d";
+		action = ''"_d'';
+	}
 
-		{
-			mode = "n";
-			key = "Q";
-			action = "<nop>";
-		}
+	{
+		mode = "n";
+		key = "Q";
+		action = "<nop>";
+	}
 
-		{
-			mode = "n";
-			key = "<leader>s";
-			action = '':%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>'';
-		}
-		{
-			mode = "n";
-			key = "<leader>x";
-			action = "<cmd>!chmod +x %<CR>";
-			options = { silent = true; };
-		}
-
-
-		];
+	{
+		mode = "n";
+		key = "<leader>s";
+		action = '':%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>'';
+	}
+	{
+		mode = "n";
+		key = "<leader>x";
+		action = "<cmd>!chmod +x %<CR>";
+		options = { silent = true; };
+	}
+	{
+		mode = "n";
+		key = "=a";
+		action = "mzgg=G'zzz";
+	}
+	];
 }
