8bb49997237237ffc6213b7fa88eb5dcf99d8eb3
Jaime Kurzweg <jkurzweg10@gmail.com>
2024-10-26 13:16:42 -0600

    readded nixlsp to nvim. switched to nixd

--- custom_flakes/nvim/plugins/cmp-lsp-ts.nix
+++ custom_flakes/nvim/plugins/cmp-lsp-ts.nix
@@ -5,34 +5,34 @@
 			settings.indent.enable = true;
 			settings.ensure_installed = [
 				"nix"
-				# "gdscript"
 			];
 		};
 
 		lsp = {
 			enable = true;
 			servers = {
-				nil_ls.enable = true;
-				# gdscript.enable = true;
+				nixd.enable = true;
 			};
 		};
 		cmp = {
 			enable = true;
 			autoEnableSources = true;
-			# sources = [
-			# {name = "nvim_lsp";}
-			# {name = "path";}
-			# {name = "luasnip";}
-			# {name = "buffer";}
-			# ];
-			settings.mapping = {
-				"<C-Space>" = "cmp.mapping.complete()";
-				"<C-d>" = "cmp.mapping.scroll_docs(-4)";
-				"<C-e>" = "cmp.mapping.abort()";
-				"<C-f>" = "cmp.mapping.scroll_docs(4)";
-				"<C-y>" = "cmp.mapping.confirm({ select = true })";
-				"<C-p>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-				"<C-n>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+			settings = {
+				sources = [
+					{name = "nvim_lsp";}
+					{name = "path";}
+					{name = "luasnip";}
+					{name = "buffer";}
+				];
+				mapping = {
+					"<C-Space>" = "cmp.mapping.complete()";
+					"<C-d>" = "cmp.mapping.scroll_docs(-4)";
+					"<C-e>" = "cmp.mapping.abort()";
+					"<C-f>" = "cmp.mapping.scroll_docs(4)";
+					"<C-y>" = "cmp.mapping.confirm({ select = true })";
+					"<C-p>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+					"<C-n>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+				};
 			};
 		};
 	};
