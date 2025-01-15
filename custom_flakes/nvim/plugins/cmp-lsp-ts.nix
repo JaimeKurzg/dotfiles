fdf3546444430c5147433f9dd9a98b2e8cd04b4b
Jaime Kurzweg <jkurzweg10@gmail.com>
2025-01-15 11:51:07 -0700

    nvim-cmp c-space starts completion

--- custom_flakes/nvim/plugins/cmp-lsp-ts.nix
+++ custom_flakes/nvim/plugins/cmp-lsp-ts.nix
@@ -35,12 +35,20 @@
 					{name = "buffer";}
 				];
 				mapping = {
-					"<C-Space>" = "cmp.mapping.confirm({ select = true })";
 					"<C-d>" = "cmp.mapping.scroll_docs(-4)";
 					"<C-e>" = "cmp.mapping.abort()";
 					"<C-f>" = "cmp.mapping.scroll_docs(4)";
 					"<C-y>" = "cmp.mapping.confirm({ select = true })";
 					"<C-p>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+					"<C-Space>" = ''cmp.mapping(function(fallback)
+								if cmp.visible() then
+									cmp.confirm({ select = true }) 
+								else
+									cmp.complete()
+								end
+							end, {'i', 's'})
+					'';
+
 					"<C-n>" = ''cmp.mapping(function(fallback)
 								if cmp.visible() then
 									cmp.select_next_item()
